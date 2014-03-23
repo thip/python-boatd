d5435772e72f74a8cf1253e0781f08f641dca551
Louis Taylor <kragniz@gmail.com>
2014-03-23 03:42:14 +0000

    Add url abstraction

--- boatd_client.py
+++ boatd_client.py
@@ -1,12 +1,24 @@
+from __future__ import print_function
+
 try:
     from urllib.request import urlopen
 except ImportError:
     from urllib2 import urlopen
 
+import json
+
 class Boat(object):
-    def __init__(self):
-        self.port = 2222
-        self.host = 'http://localhost:{}{}'
+    def __init__(self, host='localhost', port=2222):
+        self.host = host
+        self.port = port
+
+    def _url(self, endpoint):
+        return 'http://{}:{}{}'.format(self.host, self.port, endpoint)
 
     def _get(self, endpoint):
-        return json.loads(urlopen(self.host.format(self.port, endpoint)))
+        json_body = urlopen(self._url(endpoint)).read()
+        return json.loads(json_body)
+
+if __name__ == '__main__':
+    boat = Boat()
+    print(boat._get(''))
