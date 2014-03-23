23762a71abc3020684eef7f3f107ada2aa91a237
Louis Taylor <kragniz@gmail.com>
2014-03-23 03:50:07 +0000

    Add methods for the basic get functions

--- boatd_client.py
+++ boatd_client.py
@@ -19,6 +19,21 @@ class Boat(object):
         json_body = urlopen(self._url(endpoint)).read()
         return json.loads(json_body)
 
+    def heading(self):
+        content = self._get('/heading')
+        return content.get('result')
+
+    def wind(self):
+        content = self._get('/wind')
+        return content.get('result')
+
+    def position(self):
+        content = self._get('/position')
+        return tuple(content.get('result'))
+
 if __name__ == '__main__':
     boat = Boat()
     print(boat._get(''))
+    print(boat.heading())
+    print(boat.wind())
+    print(boat.position())
