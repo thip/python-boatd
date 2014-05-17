01bc75ff5db44f65851c13ed0f05fb1ac80c583c
Louis Taylor <kragniz@gmail.com>
2014-05-17 16:49:57 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='boatd_client',
+    version='0.1',
+    author='Louis Taylor',
+    author_email='kragniz@gmail.com',
+    description=('Python wrapper for the boatd API, used to write behavior scripts.'),
+    license='GPL',
+    keywords='boat sailing wrapper rest',
+    url='https://github.com/boatd/python-boatd',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    ],
+)
