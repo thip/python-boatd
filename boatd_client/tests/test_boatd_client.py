d75f30b631638f27e4ad9f9dc6a639c29b8cbe1e
Louis Taylor <kragniz@gmail.com>
2014-05-17 23:16:35 +0100

    Add heading test

--- boatd_client/tests/test_boatd_client.py
+++ boatd_client/tests/test_boatd_client.py
@@ -11,3 +11,8 @@ class TestBoatdClient(object):
         HTTPretty.register_uri(HTTPretty.GET, 'http://localhost:2222/',
                                body='{"boatd": {"version": "0.1mock"}}')
         assert self.boat.version()
+
+    def test_heading(self):
+        HTTPretty.register_uri(HTTPretty.GET, 'http://localhost:2222/heading',
+                               body='{"result": 2.43}')
+        assert self.boat.heading() == 2.43
