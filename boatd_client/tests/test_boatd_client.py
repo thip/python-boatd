b7a9d69bac2e6221d1a582a243a7ee96c2c270e8
Louis Taylor <kragniz@gmail.com>
2014-05-19 00:49:50 +0100

    Updates tests to use properties

--- boatd_client/tests/test_boatd_client.py
+++ boatd_client/tests/test_boatd_client.py
@@ -10,9 +10,14 @@ class TestBoatdClient(object):
     def test_root(self):
         HTTPretty.register_uri(HTTPretty.GET, 'http://localhost:2222/',
                                body='{"boatd": {"version": "0.1mock"}}')
-        assert self.boat.version()
+        assert self.boat.version
 
     def test_heading(self):
         HTTPretty.register_uri(HTTPretty.GET, 'http://localhost:2222/heading',
                                body='{"result": 2.43}')
-        assert self.boat.heading() == 2.43
+        assert self.boat.heading == 2.43
+
+    def test_wind(self):
+        HTTPretty.register_uri(HTTPretty.GET, 'http://localhost:2222/wind',
+                               body='{"direction": 8.42}')
+        assert self.boat.wind.direction == 8.42
