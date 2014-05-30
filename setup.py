951c79f26ced539f0a53748828ecf3679efbbabc
Louis Taylor <kragniz@gmail.com>
2014-05-30 14:59:02 +0100

    Add manifest

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='boatd_client',
-    version='0.1.3',
+    version='0.1.4',
     author='Louis Taylor',
     author_email='kragniz@gmail.com',
     description=('Python wrapper for the boatd API, used to write behavior scripts.'),
