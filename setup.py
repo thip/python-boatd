711ff98facfe7b66f5f3d1dea1d36e036ff553f1
Louis Taylor <louis@kragniz.eu>
2016-05-11 03:30:36 +0100

    Modernize time

--- setup.py
+++ setup.py
@@ -4,20 +4,16 @@ except ImportError:
     from distutils.core import setup
 
 setup(
-    name='boatd_client',
+    name='python-boatdclient',
     version='0.1.4',
     author='Louis Taylor',
-    author_email='kragniz@gmail.com',
+    author_email='louis@kragniz.eu',
     description=('Python wrapper for the boatd API, used to write behavior scripts.'),
     long_description=open("README.rst").read(),
     license='GPL',
     keywords='boat sailing wrapper rest',
     url='https://github.com/boatd/python-boatd',
-    packages=['boatd_client'],
-    requires=['docopt'],
-    install_requires=[
-        'docopt >= 0.6'
-        ],
+    packages=['boatdclient'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
