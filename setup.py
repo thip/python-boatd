a502796773b81de69b8f20f213771255dcc5dcda
Louis Taylor <louis@kragniz.eu>
2016-05-17 05:04:33 +0100

    Release 0.3.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='python-boatdclient',
-    version='0.3.0',
+    version='0.3.1',
     author='Louis Taylor',
     author_email='louis@kragniz.eu',
     description=('Python wrapper for the boatd API, used to write behavior scripts.'),
