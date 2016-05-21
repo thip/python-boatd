4f14d476f7a7cadd78b57b3332c8173b68ad8b29
Louis Taylor <louis@kragniz.eu>
2016-05-21 15:29:05 +0100

    Add some example docstring to bearing

--- boatdclient/bearing.py
+++ boatdclient/bearing.py
@@ -1,7 +1,28 @@
 import math
 
 class Bearing(object):
-    """An angle between 0 and 360 degrees"""
+    '''
+    An angle between 0 and 360 degrees
+
+    Examples:
+
+    >>> Bearing(100)
+    <Bearing (100.00 degrees clockwise from north) at 0x7f25e22b3710>
+    >>> Bearing(100) + Bearing(100)
+    <Bearing (200.00 degrees clockwise from north) at 0x7f25e22b3940>
+    >>> Bearing(100) + Bearing(300)
+    <Bearing (40.00 degrees clockwise from north) at 0x7f25e22b37b8>
+    >>> Bearing(0) - Bearing(100)
+    <Bearing (260.00 degrees clockwise from north) at 0x7f25e22b3940>
+    >>> import math
+    >>> Bearing.from_radians(math.pi)
+    <Bearing (180.00 degrees clockwise from north) at 0x7f25e22b3828>
+    >>> int(Bearing(120.4))
+    120
+    >>> float(Bearing(120.4))
+    120.4
+    '''
+
     def __init__(self, degrees):
         self._degrees = float(degrees % 360)
 
@@ -30,6 +51,10 @@ class Bearing(object):
 
     def __str__(self):
         return '{0:0.2f} degrees clockwise from north'.format(self.degrees)
+
+    def __repr__(self):
+        return '<{0}.{1} ({2}) at {3}>'.format(
+            self.__module__, type(self).__name__, str(self), hex(id(self)))
 
     def __int__(self):
         return int(self._degrees)
