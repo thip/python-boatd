9650a456f82e04b7ff150b19e879a79375d130ad
Louis Taylor <louis@kragniz.eu>
2016-06-07 04:35:10 +0100

    Add get_current_waypoints to __init__

--- boatdclient/__init__.py
+++ boatdclient/__init__.py
@@ -1,6 +1,6 @@
 from .boatd_client import Boat  # noqa
 from .boatd_client import Behaviour  # noqa
-from .boatd_client import Waypoints  # noqa
+from .boatd_client import get_current_waypoints  # noqa
 
 from .point import Point  # noqa
 from .bearing import Bearing  # noqa
